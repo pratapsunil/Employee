07a911fabed8eaa46ed995dcb7f9a749e8e631d8
PRATAP <PRATAP@192.168.0.13>
2017-06-19 17:25:47 +0530

    first code

---
+++ src/Emp.java
@@ -0,0 +1,18 @@
+
+public class Emp {
+	private int empName;
+
+	public int getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(int empName) {
+		this.empName = empName;
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [empName=" + empName + "]";
+	}
+
+}
